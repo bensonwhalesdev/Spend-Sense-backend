e7e7ccbac4fd809f8478329110134a4d64f3ec17
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-07-20 12:17:52 -0700

    file updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://spend-sense-six.vercel.app",
     credentials: true,
   }));
 app.use(express.json());
