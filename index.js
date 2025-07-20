e12c23df9a76664409e28f48be61653e5df457c2
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-07-20 13:05:57 -0700

    files updated

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
+const connectDB = require('./src/config/db');
 const usersRouter = require('./src/routes/user.route');
 const authRouter = require('./src/routes/auth.routes');
 const accountRoutes = require('./src/routes/account.route');
 const budgetRoutes = require('./src/routes/budget.route');
 const monthlyBudgetRoutes = require('./src/routes/monthlybudget.route');
 const existingUserRoutes = require('./src/routes/existing-user.route');
+
 const app = express();
-const PORT = 5000;
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Spend sense!');
-});
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://spend-sense-six.vercel.app",
-    credentials: true,
-  }));
+  origin: "https://spend-sense-six.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/v1/auth', authRouter)
+
+app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', usersRouter);
 app.use("/api/v1/accounts", accountRoutes);
 app.use("/api/v1/budgets", budgetRoutes);
 app.use("/api/v1/monthlybudgets", monthlyBudgetRoutes);
 app.use("/api/v1/existingusers", existingUserRoutes);
 
-
-async function connectDB() {
-   try {
-        const con = await mongoose.connect(process.env.MONGO_URI);
-        console.log('database succesful');  
-        app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Hello Spend Sense!');
 });
-    } catch (error) {
-        console.log(error);   
-    }
-}
-connectDB();
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
